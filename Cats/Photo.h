c9cbd02d8c0f8fa67f3eed3577db6bb6b58f1d61
olga-nest <olga.w.nesterova@gmail.com>
2017-10-23 13:23:08 -0700

    Create Photo class

---
+++ Cats/Photo.h
@@ -0,0 +1,16 @@
+//
+//  Photo.h
+//  Cats
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+
+@property (nonatomic) NSString *photoTitle;
+@property (nonatomic) NSURL *photoURL;
+
+@end
---
+++ Cats/Photo.m
@@ -0,0 +1,13 @@
+//
+//  Photo.m
+//  Cats
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+
+@end
