2afbee7db8642650412411ac295fd670612aebdd
olga-nest <olga.w.nesterova@gmail.com>
2017-10-23 13:24:20 -0700

    Move GET request to its own module

--- Cats/ViewController.m
+++ Cats/ViewController.m
@@ -16,10 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self getCatPictures];
     
+}
+
+-(void)getCatPictures {
     NSString *dataUrl = @"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=b066df57d7b11069504a3b0819a67999&tags=cat";
     NSURL *url = [NSURL URLWithString:dataUrl];
-
+    
     NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
     [urlRequest setHTTPMethod:@"GET"];
     
@@ -31,38 +35,36 @@
                                                 completionHandler:^(NSData *_Nullable data,
                                                                     NSURLResponse *_Nullable response,
                                                                     NSError *_Nullable error) {
-       
-
-        if (error) {
-            NSLog(@"Error getting data");
-        } else {
-            NSLog(@"RESPONSE: %@", response);
-            NSLog(@"DATA: %@", data);
-            
-            NSError *jsonError = nil;
-            NSDictionary *readDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-            NSDictionary *secondDict = readDict.allValues[1];
-            
-            if (jsonError) {
-                NSLog(@"jsonError: %@", jsonError.localizedDescription);
-            } else {
-                 NSLog(@"There are: %lu objects in readDict", (unsigned long)secondDict.count);
-                
-                NSArray *photosArray = [secondDict objectForKey:@"photo"];
-                NSLog(@"photosArray contains %lu objects", photosArray.count);
-            }
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                
-            });
-        }
-        
-    }];
+                                                    
+                                                    
+                                                    if (error) {
+                                                        NSLog(@"Error getting data");
+                                                    } else {
+                                                        NSLog(@"RESPONSE: %@", response);
+                                                        NSLog(@"DATA: %@", data);
+                                                        
+                                                        NSError *jsonError = nil;
+                                                        NSDictionary *readDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                                                        NSDictionary *secondDict = readDict.allValues[1];
+                                                        
+                                                        if (jsonError) {
+                                                            NSLog(@"jsonError: %@", jsonError.localizedDescription);
+                                                        } else {
+                                                            NSLog(@"There are: %lu objects in readDict", (unsigned long)secondDict.count);
+                                                            
+                                                            NSArray *photosArray = [secondDict objectForKey:@"photo"];
+                                                            NSLog(@"photosArray contains %lu objects", photosArray.count);
+                                                        }
+                                                        
+                                                        dispatch_async(dispatch_get_main_queue(), ^{
+                                                            
+                                                        });
+                                                    }
+                                                    
+                                                }];
     
-     [dataTask resume];
+    [dataTask resume];
 }
-
-
 
 
 
