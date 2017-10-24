22a463a062874745baabe38813b3d43c30eafc59
olga-nest <olga.w.nesterova@gmail.com>
2017-10-24 14:41:54 -0700

    Add coordinate property to Photo class

--- Cats/Photo.h
+++ Cats/Photo.h
@@ -8,13 +8,15 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface Photo : NSObject
+@interface Photo : NSObject <MKAnnotation>
 
 @property (nonatomic) NSString *photoTitle;
 @property (nonatomic) NSURL *photoURL;
 @property (nonatomic) UIImage *image;
 @property (nonatomic) NSString *photoId;
+@property (nonatomic) CLLocationCoordinate2D coordinate;
 
 - (instancetype)initWithImage: (NSURL *) url andTitle: (NSString *)title andId: (NSString *) photoId;
 
