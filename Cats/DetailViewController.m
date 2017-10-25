50dc3d49af60772b2e4d02ee02de47923867459d
olga-nest <olga.w.nesterova@gmail.com>
2017-10-24 17:59:27 -0700

    Get Photo Location Data

--- Cats/DetailViewController.m
+++ Cats/DetailViewController.m
@@ -10,6 +10,8 @@
 
 @interface DetailViewController ()
 
+//@property (nonatomic) LocationManager *locationManager;
+
 @end
 
 @implementation DetailViewController
@@ -19,6 +21,7 @@ static NSString * const reuseIdentifier = @"Cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self configureView];
+    [self getPhotoLocationData];
     
     }
 
@@ -37,6 +40,64 @@ static NSString * const reuseIdentifier = @"Cell";
         // Update the view.
         [self configureView];
     }
+}
+
+
+-(void)getPhotoLocationData
+    {
+        NSString *urlString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=b066df57d7b11069504a3b0819a67999&photo_id=%@&format=json&nojsoncallback=1&", self.photo.photoId];
+        
+        NSURL *photoURL = [NSURL URLWithString:urlString];
+        
+        NSURLRequest *urlRequest = [NSURLRequest requestWithURL:photoURL];
+        
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        
+        NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *_Nullable data,
+                                                                                                     NSURLResponse *_Nullable response,
+                                                                                                     NSError *_Nullable error) {
+            
+            if (error) {
+                NSLog(@"Error getting data: %@", error.localizedDescription);
+            } else {
+                NSLog(@"RESPONSE: %@", response);
+                NSLog(@"DATA: %@", data);
+                
+                NSError *jsonError = nil;
+                
+                //get all values from top-level Dictionary
+                NSDictionary *readDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                NSDictionary *secondDict = readDict.allValues[0];
+                
+                if (jsonError) {
+                    NSLog(@"jsonError: %@", jsonError.localizedDescription);
+                } else {
+                    
+                    //Dictionarie with all photo location Data
+                    NSDictionary *locationDataDictionary = secondDict.allValues[1];
+                    
+                    double lat = [[locationDataDictionary objectForKey:@"latitude"]integerValue];
+                    double lon = [[locationDataDictionary objectForKey:@"longitude"]integerValue];
+                    
+                    CLLocationCoordinate2D coordinates = CLLocationCoordinate2DMake(lat, lon);
+                    self.photo.coordinate = coordinates;
+                    MKCoordinateSpan span = MKCoordinateSpanMake(.5f, .5f);
+                    self.detailMapView.region = MKCoordinateRegionMake(self.photo.coordinate, span);
+                    
+                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                        [self.detailMapView addAnnotation:self.photo];
+                    }];
+                    
+                }
+            }
+            
+        }];
+        
+        [dataTask resume];
+        
+        //    return nil;
+  //  }
 }
 
 @end
