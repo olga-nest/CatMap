123560a18aacaed00e2eb6e419231fc45044b384
olga-nest <olga.w.nesterova@gmail.com>
2017-10-24 18:00:23 -0700

    Do not save photo location when creating photo object

--- Cats/ViewController.m
+++ Cats/ViewController.m
@@ -122,14 +122,14 @@
                                                                 //reuse objectID
                                                                 
                                                                 //get photo location
-                                                                double lon = [[object objectForKey:@"longitude"]integerValue];
-                                                                double lat = [[object objectForKey:@"latitude"]integerValue];
-                                                                
-                                                                CLLocationCoordinate2D photoLocation = CLLocationCoordinate2DMake(lat, lon);
-                                                                
+//                                                                double lon = [[object objectForKey:@"longitude"]integerValue];
+//                                                                double lat = [[object objectForKey:@"latitude"]integerValue];
+//
+//                                                                CLLocationCoordinate2D photoLocation = CLLocationCoordinate2DMake(lat, lon);
+//
                                                                 //instantiate Photo
                                                                 Photo *photo = [[Photo alloc]initWithImage:url andTitle:objectTitle andId:objectID];
-                                                                photo.coordinate = photoLocation;
+//                                                                photo.coordinate = photoLocation;
                                                                 
                                                                 //add to array
                                                                 [self.allPhotos addObject:photo];
