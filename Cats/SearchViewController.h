beb856b3e58d3b6f7670019f3f152ee6a7e075d6
olga-nest <olga.w.nesterova@gmail.com>
2017-10-24 19:00:09 -0700

    Add searchViewController

---
+++ Cats/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Cats
+//
+//  Created by Olga on 10/24/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ Cats/SearchViewController.m
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.m
+//  Cats
+//
+//  Created by Olga on 10/24/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+   }
+
+
+@end
